01f713cc46c8f19c35b3549c9348026120204164
Drajat Hasan <drajathasan20@gmail.com>
2022-06-09 07:52:09 +0700

    Update factory flow

--- src/Pdf/Factory.php
+++ src/Pdf/Factory.php
@@ -3,7 +3,7 @@
  * @author Drajat Hasan
  * @email drajathasan20@gmail.com
  * @create date 2022-02-22 14:01:56
- * @modify date 2022-02-22 14:01:56
+ * @modify date 2022-06-09 07:48:49
  * @license GPLv3
  * @desc [description]
  */
@@ -46,17 +46,32 @@ class Factory
     private $Template;
 
     
-    public function __construct(object $template)
+    public function __construct($template = '')
     {
-        if (!empty($template)) 
+        if (is_object($template))
         {
-            // Set template instance
-            $this->Template = $template;
-
+            // set template
+            $this->setTemplate($template);
             // set provider
-            $Class = $this->PrefixNamespace . $template->getProvider();
-            $this->Provider = new $Class;
+            $this->setProvider($this->Template->getProvider());
         }
+    }
+
+    /**
+     * Method to set template instance
+     *
+     * @param object $template
+     * @return void
+     */
+    public function setTemplate(object $template)
+    {
+        if (!$template instanceof TemplateContract) 
+        {
+            throw new Exception("Provider {$providerNameOrClass} not fullable template contract!", 1);
+        }
+
+        // Set template instance
+        $this->Template = $template;
     }
 
     /**
@@ -65,14 +80,20 @@ class Factory
      * @param string $providerName
      * @return void
      */
-    public function setProvider(string $providerName)
-    {
-        $Class = $this->PrefixNamespace . $providerName;
-
-        if (!class_exists($Class)) 
-            throw new Exception("Provider {$providerName} not found!", 1);
-
-        $this->Provider = new $Class;
+    public function setProvider(string $providerNameOrClass)
+    {            
+        if (class_exists($Class = $this->PrefixNamespace . $providerNameOrClass))
+        {
+            $this->Provider = new $Class;    
+        }
+        else if (class_exists($providerNameOrClass))
+        {
+            $this->Provider = new $providerNameOrClass;
+        }
+        else
+        {
+            throw new Exception("Provider {$providerNameOrClass} not found!", 1);
+        }
 
         return $this;
     }
